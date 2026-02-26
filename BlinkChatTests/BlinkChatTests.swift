50041677c73581c8dcdd4e8816f010b5ef439c92
Joe Williams <joewills13@icloud.com>
2026-02-26 21:09:40 +0000

    Initial Project

---
+++ BlinkChatTests/BlinkChatTests.swift
@@ -0,0 +1,17 @@
+//
+//  BlinkChatTests.swift
+//  BlinkChatTests
+//
+//  Created by Joe on 26/02/2026.
+//
+
+import Testing
+@testable import BlinkChat
+
+struct BlinkChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ BlinkChatUITests/BlinkChatUITests.swift
@@ -0,0 +1,27 @@
+import XCTest
+
+final class BlinkChatUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    override func tearDownWithError() throws { }
+
+    @MainActor
+    func testExample() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    @MainActor
+    func testLaunchPerformance() throws {
+        // This measures how long it takes to launch your application.
+        measure(metrics: [XCTApplicationLaunchMetric()]) {
+            XCUIApplication().launch()
+        }
+    }
+}
